910ed10d28afaf1dd28918b8ef741be485cafefb
张兆玉 <1983909571@qq.com>
2023-12-07 21:57:20 +0800

    23-12-07 bugs-1

--- src/main/java/lanqiao/lq_7940_最后的元音.java
+++ src/main/java/lanqiao/lq_7940_最后的元音.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 //倒叙暴力查找
 public class lq_7940_最后的元音 {
     public static void main(String[] args) {
-        public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            String  str  = scan.next();
-            for(int i=str.length()-1;i>=0;i--){
-                if(isVowel(str.charAt(i))){
-                    System.out.print(str.charAt(i));
-                    break;
-                }
+        Scanner scan = new Scanner(System.in);
+        String  str  = scan.next();
+        for(int i=str.length()-1;i>=0;i--){
+            if(isVowel(str.charAt(i))){
+                System.out.print(str.charAt(i));
+                break;
             }
-            scan.close();
         }
+        scan.close();
+    }
 
-        public static boolean isVowel(char s){
-            if(s=='a' || s=='e' || s=='i' || s=='o' || s=='u')
-                return true;
-            else
-                return false;
-        }
+    public static boolean isVowel(char s){
+        if(s=='a' || s=='e' || s=='i' || s=='o' || s=='u')
+            return true;
+        else
+            return false;
     }
 }
